d87f1d904adbc76a3a1e78a5b2ecb04e2c77de8e
emefienem <emefienemmichael@gmail.com>
2023-11-20 16:37:59 -0800

    added pwa

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+// service-worker.js
+const CACHE_NAME = 'burger-website';
+const urlsToCache = [
+  '/index.html',
+  '/menu.html',
+  '/shop.html',
+  '/contact.html',
+  '/cart.html',
+  '/about.html',
+  '/styles.css',
+  '/script.js',
+  'images/offline-logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('images/offline-logo.png')
+      });
+    })
+  );
+});
