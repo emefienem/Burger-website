897e56cfb31114103c8acfaf84dda5240a11333a
emefienem <emefienemmichael@gmail.com>
2023-11-25 20:28:50 -0800

    added offline page

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,7 @@ const urlsToCache = [
   '/contact.html',
   '/cart.html',
   '/about.html',
+  'offline.html',
   '/styles.css',
   '/script.js',
   'images/offline-logo.png'
@@ -24,7 +25,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).catch(() => {
-        return caches.match('images/offline-logo.png')
+        return caches.match('offline.html')
       });
     })
   );
